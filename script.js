202c5d9d763c458128bd67d4376365b9b296beac
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-25 14:41:53 +0100

    Create cell class to turn to black

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+//create button add more than one grid, to fix
+//new grid onclick button
+
 const gridButton = document.getElementById('createGrid');
 const container = document.querySelector('.grid');
 
 
 gridButton.addEventListener('click', () => {
-    for (let x = 0; x < 4; x++) {
-        for (let y = 0; y < 4; y++) {
+    let gridSize = 16;
+    for (let x = 0; x < gridSize; x++) {
+        for (let y = 0; y < gridSize; y++) {
             const newCell = document.createElement('div');
-            newCell.classList.add('.cell');
-            newCell.style.width = 'calc(700px / 4)';
-            newCell.style.height = 'calc(700px / 4)';
-            newCell.style.background = 'lightblue';
+            newCell.classList.add('cell');
+            newCell.style.width = `calc(700px / ${gridSize})`;
+            newCell.style.height = `calc(700px / ${gridSize})`;
             container.appendChild(newCell);
 
         }
     }
 });
+
+container.addEventListener('mousedown', (e) => {
+    let cell = e.target.closest('div');
+
+    if (!cell) return;
+
+    cell.classList.add('black');
+})
