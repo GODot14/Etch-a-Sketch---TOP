a5ac85b5d3afcd87228b48fa3d5195316ab8d369
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-27 22:40:03 +0100

    Add choice of grid size

--- script.js
+++ script.js
@@ -3,10 +3,22 @@
 
 const gridButton = document.getElementById('createGrid');
 const container = document.querySelector('.grid');
+const clear = document.querySelector('#clear');
+const gridSizeChoice = document.getElementById('gridSize');
 
+let gridSize = 16;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+gridSizeChoice.addEventListener('click', () => {
+    gridSize = window.prompt('Please, choose a grid size. Default is 16*16');
+    if (gridSize < 4 || gridSize > 100) return gridSize = 16;
+    return gridSize;
+})
 
 gridButton.addEventListener('click', () => {
-    let gridSize = 16;
     for (let x = 0; x < gridSize; x++) {
         for (let y = 0; y < gridSize; y++) {
             const newCell = document.createElement('div');
@@ -19,10 +31,19 @@ gridButton.addEventListener('click', () => {
     }
 });
 
-container.addEventListener('mousedown', (e) => {
+
+container.addEventListener('mouseover', (e) => {
     let cell = e.target.closest('div');
-
+    
     if (!cell) return;
-
+    if (e.type === 'mouseover' && !mouseDown) return;
     cell.classList.add('black');
+})
+
+clear.addEventListener('click', () => {
+    const cells = document.querySelectorAll('.cell');
+    
+    cells.forEach((cell) => {
+        cell.classList.remove('black');
+     })
 })
